29af6044540c9e6e4695b386d052de0787f9250b
unknown <deepanshugulia256@gmail.com>
2022-09-07 14:04:19 +0530

    deploying on json server

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
+// importing json-server
 const jsonServer = require("json-server");
+
+// getting all required routes and middleware from json-server
 const server = jsonServer.create();
 const router = jsonServer.router("weather.json");
 const middlewares = jsonServer.defaults();
+
+// port for running server
 const port = process.env.PORT || 8080;
 
+// applying routes and middleware
 server.use(middlewares);
 server.use(router);
 
+// starting server
 server.listen(port);
